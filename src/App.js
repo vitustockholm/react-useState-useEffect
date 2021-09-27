c41590818ad344e6d1d02a243fb50a1d1d3c10af
Vitus <vitustockholm@gmail.com>
2021-09-27 12:23:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'; //usestate for manage state ,useefect for fetch data
+import axios from 'axios'; //for fetch data
+
 import './App.css';
+//styling divs with fetched user info from API
+// const styles = {
+//   display: 'inline',
+//   width: '30%',
+//   height: 95,
+//   float: 'left',
+//   padding: 3,
+//   border: '0.5px solid black',
+//   marginBottom: 7,
+//   marginRight: 7,
+// };
 
 function App() {
+  const [allData, setAllData] = useState([]); //holds info from api
+  const [filteredData, setFilteredData] = useState(allData); //holds a copy after searchable word has value=>statechanges
+
+  //search logica
+
+  const handleSearch = (event) => {
+    let value = event.target.value.toLowerCase();
+    // value.toLowerCase();
+    let result = [];
+    console.log('value', value);
+    result = allData.filter((data) => {
+      return (
+        data.username.search(value) !== -1 || data.name.search(value) !== -1
+      );
+    });
+    setFilteredData(result);
+  };
+
+  useEffect(() => {
+    //side effect
+    axios('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        console.log(response.data);
+        setAllData(response.data);
+        setFilteredData(response.data);
+      })
+      .catch((error) => {
+        console.log('Error bad data: ', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ margin: '0 auto', marginTop: '2%' }}>
+        <label>Search: </label>
+        <br />
+        <input type='text' onChange={(event) => handleSearch(event)} />
+      </div>
+      <div style={{ padding: 10 }}>
+        {filteredData.map((value, index) => {
+          return (
+            // style={styles}
+            <div key={value.id}>
+              <p>
+                ID:
+                {value.id} <br /> <u>Name</u>: {value.name}
+              </p>
+              <p>
+                <u>Username :</u> {value.username}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
